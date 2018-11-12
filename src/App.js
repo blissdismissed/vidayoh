e75a72ba33ec38f23422efdf28eea154d0748f9d
Ish <Ish@MacBook-Pro-de-Ish.local>
2018-11-11 22:10:37 -0500

    check if coveralls is working

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default () => (
   <div className="App">
     <header className="App-header">
       <h1 className="App-title">Welcome To Gaijins Always Lose.</h1>
+        <p>In this lesson you will learn how to properly put water into the offering.</p>
     </header>
     <p className="App-intro">
       My name is Gaijin. Welcome, and hello!
