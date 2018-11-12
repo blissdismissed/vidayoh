b70d4b3bd91a68748ed64861d537761d1ee2a5ca
Ish <Ish@MacBook-Pro-de-Ish.local>
2018-11-11 20:30:15 -0500

    Add the travis file and homepage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactDOM from 'react-dom';
-import './index.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Gaijins Always Lose
-          </p>
-        </header>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Gaijins Always Lose
+//           </p>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
+
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome To Gaijins Always Lose.</h1>
+    </header>
+    <p className="App-intro">
+      My name is Gaijin. Welcome, and hello!
+    </p>
+  </div>
+);
