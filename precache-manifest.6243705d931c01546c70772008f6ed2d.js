d8bab760ad05d942b809e5765dedac25047c3a95
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-12 03:26:29 +0000

    Deploy blissdismissed/vidayoh to github.com/blissdismissed/vidayoh.git:gh-pages

---
+++ precache-manifest.6243705d931c01546c70772008f6ed2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2641ef8ea005c20a821a",
+    "url": "/vidayoh/static/css/main.9818b630.chunk.css"
+  },
+  {
+    "revision": "2641ef8ea005c20a821a",
+    "url": "/vidayoh/static/js/main.2641ef8e.chunk.js"
+  },
+  {
+    "revision": "e972a97fedc948707364",
+    "url": "/vidayoh/static/js/1.e972a97f.chunk.js"
+  },
+  {
+    "revision": "f577c462f82a4107bd42",
+    "url": "/vidayoh/static/js/runtime~main.f577c462.js"
+  },
+  {
+    "revision": "9fdd41eebf86be768fc7286b5bfd6e44",
+    "url": "/vidayoh/index.html"
+  }
+];
