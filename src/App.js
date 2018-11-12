c087fb6d8fc21b3c194798a492d34f1385814ecb
Ish <Ish@MacBook-Pro-de-Ish.local>
2018-11-11 21:36:43 -0500

    Remove unused variables in index.js and app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Gaijins Always Lose
-//           </p>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
 
 export default () => (
   <div className="App">
